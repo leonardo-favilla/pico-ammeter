ae948bf4c99227fcdb238c6eba67b21291a8cbfc
leonardo-favilla <leonardo.favilla@cern.ch>
2025-03-26 15:12:19 +0100

    removed hello-world

--- hello.py
+++
@@ -1,2 +0,0 @@
-import datetime
-print(f"Hello! The time is {datetime.datetime.now()}")
--- runner.sh
+++ runner.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
 while true; do
-    # python3 hello.py >> log.log
     python3 Pico_reader_converter.py -t 300 -w -r -f /eos/user/l/lfavilla/GEM/MagnetTest3_Apr2025/picoData_v2 >> log_pico.log
     sleep 300
 done
