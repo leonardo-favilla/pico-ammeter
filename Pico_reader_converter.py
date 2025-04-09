c8be9bed22cc895c90e462c580744439d64920c4
leonardo-favilla <leonardo.favilla@cern.ch>
2025-04-09 09:54:44 +0200

    modified rate sent to grafana 10Hz -> 4Hz

--- Pico_reader_converter.py
+++ Pico_reader_converter.py
@@ -658,7 +658,7 @@ while (time.time() - t0 <= time_acq/time_divider) or (len(bytes)>0):
 
 
             # point = Point("current_measurement").tag("channel", "G3B").field("current", curr[0]).time(exact_time_s, write_precision="s")
-            if nev%40 == 0:
+            if nev%100 == 0:
                 # point_I = Point("current_measurement").field("I_G3B", curr[0]).field("I_G3T", curr[1]).field("I_G2B", curr[2]).field("I_G2T", curr[3]).field("I_G1B", curr[4]).field("I_G1T", curr[5]).field("I_DRIFT", curr[6]).time(exact_time_s, write_precision="s")
                 point_I = Point("current_measurement").field("I_G3B", curr[0]).field("I_G3T", curr[1]).field("I_G2B", curr[2]).field("I_G2T", curr[3]).field("I_G1B", curr[4]).field("I_G1T", curr[5]).field("I_DRIFT", curr[6]).time(exact_time_ms, write_precision="ms")
 
@@ -678,7 +678,7 @@ while (time.time() - t0 <= time_acq/time_divider) or (len(bytes)>0):
                 write_api.write(bucket=INFLUXDB_BUCKET, record=point_T)
 
             # send_to_influxdb(
-            #     url=INFLUXDB_URL,
+            #     url=INFLUXDB_URL,Is
             #     token=INFLUXDB_TOKEN,
             #     org=INFLUXDB_ORG,
             #     bucket=INFLUXDB_BUCKET,
